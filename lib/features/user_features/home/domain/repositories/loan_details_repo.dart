3e46c52dcc48fba879fd94dfaa5ec65590c67995
ahmed abdallah <ahmedabdallah11186@gmail.com>
2024-11-21 16:20:32 +0200

    ..

--- lib/features/user_features/home/domain/repositories/loan_details_repo.dart
+++
@@ -1,8 +0,0 @@
-import 'package:fpdart/fpdart.dart';
-import 'package:taiseer/features/user_features/home/domain/entities/loan_details_entity.dart';
-
-import '../../../../../core/errors/failure.dart';
-
-abstract class LoanDetailsRepo {
-  Future<Either<Failure, LoanDetailsEntity>> getLoanDetails(int id);
-}
